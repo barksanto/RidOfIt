467d2d4e4f4325e8b23e3a193cbee03fa8cdd5f7
Barkley <barkley.santo@gmail.com>
2022-08-08 22:55:13 +0800

    callback arrow fn on text

--- App.js
+++ App.js
@@ -1,25 +1,36 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, SafeAreaView } from "react-native"
 
 export default function App() {
   console.log("app executed")
 
   // let x
-
+  const handleTextPress = () => console.log("text pressed")
+  
   // x.toString()
   return (
-    <View style={styles.container}>
-      <Text>App.js component</Text>
+    // SafeAreaView adds padding to top of component
+    <SafeAreaView style={styles.container}>
+      <Text
+        numberOfLines={1}
+        onPress={() => {
+          handleTextPress()
+        }}
+      >
+        App.js component lease feel free to make this into a Google Doc - I just
+        know you have preferred format so defer lease feel free to make this
+        into a Google Doc
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "dodgerblue",
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
   },
 })
